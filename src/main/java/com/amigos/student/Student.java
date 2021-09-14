ae7e24c33a64c62813bd80629ee6ffacf827fb78
wonka <dubravkop@blic.net>
2021-09-14 19:50:32 +0200

    gotova klasa student

--- src/main/java/com/amigos/student/Student.java
+++ src/main/java/com/amigos/student/Student.java
@@ -9,6 +9,28 @@ public class Student {
 	private String email;
 	private LocalDate dob;
 	private Integer age;
+	
+	public Student() {
+		
+	};
+	
+	public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.dob = dob;
+		this.age = age;
+	}
+	
+	public Student(String name, String email, LocalDate dob, Integer age) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.dob = dob;
+		this.age = age;
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -38,6 +60,15 @@ public class Student {
 	}
 	public void setAge(Integer age) {
 		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", "
+				+ "name=" + name + ","
+						+ " email=" + email + ", "
+								+ "dob=" + dob + ", "
+										+ "age=" + age + "]";
 	}
 	
 	
