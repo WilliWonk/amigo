751455fd6b557e59d88ee2f93cb5a6f0165d3706
wonka <dubravkop@blic.net>
2021-09-17 08:40:19 +0200

    stigao do linija 02 promjenjena

--- src/main/java/com/amigos/AmigosCodeFirstAppApplication.java
+++ src/main/java/com/amigos/AmigosCodeFirstAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AmigosCodeFirstAppApplication {
-
+	//ovo je pocetak aplikacije
 	public static void main(String[] args) {
 		SpringApplication.run(AmigosCodeFirstAppApplication.class, args);
 	}
